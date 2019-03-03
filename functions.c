b102b2dbda8c30f4a91ad72749891b4619c7a082
eliass1997ok <gabriel.gaete.l@usach.cl>
2019-03-03 04:00:51 -0300

    Add a new dish already done. Fixes on file reading.

--- functions.c
+++ functions.c
@@ -1,5 +1,23 @@
 #include "functions.h"
 
+void agregarNodo(ListaEnlazadaLinea* platos, NodoLinea* plato){
+	if (platos->largo == 0){
+		platos->primero = plato;
+		platos->ultimo = plato;
+	} else {
+		platos->ultimo->siguiente = plato;
+		platos->ultimo = plato;
+	}
+
+	platos->largo++;
+}
+
+void addJump(ListaEnlazadaLinea* listaPlatos){
+	char* token;
+	token = strtok(listaPlatos->ultimo->ingredientes[listaPlatos->ultimo->cantidadIngredientes-1], "\n");
+	if (token) strcat(listaPlatos->ultimo->ingredientes[listaPlatos->ultimo->cantidadIngredientes-1], "\n");
+}
+
 void agregarLinea(ListaEnlazadaLinea* lineas, char* linea){
 	NodoLinea* nodo;
 	nodo = (NodoLinea*)calloc(1, sizeof(NodoLinea));
@@ -28,15 +46,8 @@ void agregarLinea(ListaEnlazadaLinea* lineas, char* linea){
 		strcpy(nodo->ingredientes[i], token);
 	}
 
-	if (lineas->largo == 0){
-		lineas->primero = nodo;
-		lineas->ultimo = nodo;
-	} else {
-		lineas->ultimo->siguiente = nodo;
-		lineas->ultimo = nodo;
-	}
-
-	lineas->largo++;
+	agregarNodo(lineas, nodo);
+	addJump(lineas);
 }
 
 ListaEnlazadaLinea* leerArchivo(char* nombreArchivo){
@@ -53,10 +64,11 @@ ListaEnlazadaLinea* leerArchivo(char* nombreArchivo){
 	char buffer[255];
 
 	while (fgets(buffer, 255, (FILE*)f)){
-		NodoLinea* nodo;
-		nodo = (NodoLinea*)calloc(1, sizeof(NodoLinea));
-
-		agregarLinea(lineas, buffer);
+		if (strcmp(buffer, "\n") != 0){
+			NodoLinea* nodo;
+			nodo = (NodoLinea*)calloc(1, sizeof(NodoLinea));
+			agregarLinea(lineas, buffer);
+		}
 	}
 
 	return lineas;
@@ -74,9 +86,43 @@ void imprimirPlatos(ListaEnlazadaLinea* listaPlatos){
 		for (int i=0; i<aux->cantidadIngredientes; i++) printf(" %s ", aux->ingredientes[i]);
 
 		printf("\n");
+
 		aux = aux->siguiente;
 	}
 
+	// printf("\n");
+}
+
+void agregarPlato(ListaEnlazadaLinea* listaPlatos){
+	NodoLinea* nuevoPlato;
+	char* nombrePlato;
+	int calorias, cantidadIngredientes;
+	
+	nombrePlato = (char*)calloc(64, sizeof(char));
+	nuevoPlato = (NodoLinea*)calloc(1, sizeof(NodoLinea));
+	nuevoPlato->comida = (char*)calloc(64, sizeof(char));
+
+	printf("Nombre del nuevo plato: ");
+	scanf("%[^\n]%*c", nombrePlato);
+	strcpy(nuevoPlato->comida, nombrePlato);
+
+	printf("Cantidad de calorías: ");
+	scanf("%d", &calorias);
+	nuevoPlato->calorias = calorias;
+
+	printf("Cantidad de Ingredientes: ");
+	scanf("%d", &cantidadIngredientes);
+	getchar();
+	nuevoPlato->cantidadIngredientes = cantidadIngredientes;
+
+	nuevoPlato->ingredientes = (char**)calloc(cantidadIngredientes, sizeof(char*));
+	for (int i=0; i<cantidadIngredientes; i++) {
+		nuevoPlato->ingredientes[i] = (char*)calloc(64, sizeof(char));
+		printf("Ingrese ingrediente número %d: ", i+1);
+		scanf("%[^\n]%*c", strcat(nuevoPlato->ingredientes[i], "\n"));
+	}
+
+	agregarNodo(listaPlatos, nuevoPlato);
 }
 
 void ejecutar(){
@@ -84,4 +130,25 @@ void ejecutar(){
 	
 	archivo = leerArchivo("foodDatabase.txt");
 	imprimirPlatos(archivo);
+	agregarPlato(archivo);
+	imprimirPlatos(archivo);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
